bd494f3a89b652808b735964a8924e8c69077346
Konstantin S. <k_schiwarov@yahoo.de>
2026-07-03 21:32:29 +0200

    update main page layout and title

--- src/app/page.js
+++ src/app/page.js
@@ -20,14 +20,11 @@ const projects = [
 export default function Home() {
   return (
     // The dark wrapper for the whole page
-    <main className="min-h-screen bg-[rgb(30,30,30)] text-white p-10">
+    <main className="min-h-screen bg-- text-white p-10">
       
-      {/* Title */}
-      <h1 className="text-4xl font-bold mb-20 text-center">
-        Selected Works
+      <h1 className="text-6xl text-primary font-bold mb-20 text-center">
+        Projects
       </h1>
-
-      {/* Here is where our magic Zick-Zack loop will go in the next step */}
       
     </main>
   );
