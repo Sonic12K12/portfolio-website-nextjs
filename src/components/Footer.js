5f5efbd15ff7f19d475663511f4923f7f33c02bc
Konstantin Schiwarov <k_schiwarov@yahoo.de>
2026-07-09 15:06:02 +0200

    add basic Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-muted">
+      <div className="text-center">
+        <p>© 2026 Konstantin Schiwarov</p>
+      </div>
+    </footer>
+  );
+}
