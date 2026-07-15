7808b2a371f9d08dc3bd732e736dd7791bad9240
Konstantin S. <k_schiwarov@yahoo.de>
2026-07-15 23:11:57 +0200

    add base footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,30 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+const currentYear = new Date().getFullYear();
+
 export default function Footer() {
   return (
-    <footer className="text-muted">
-      <div className="text-center">
-        <p>© 2026 Konstantin Schiwarov</p>
+    <footer className="text-muted py-8">
+      <hr className="border-t border-muted w-11/12 mx-auto my-8" />
+      <div className="flex justify-center gap-12 w-full px-4">
+        <a>Imprint</a>
+        <a>Privacy Policy</a>
+        <a>Contact</a>
+      </div>
+
+      <div className="flex justify-center gap-8 mt-4">
+        <a href="https://github.com/Sonic12K12">
+          <FaGithub size={26} />
+        </a>
+
+        <a href="https://www.linkedin.com/in/konstantin-schiwarov/">
+          <FaLinkedin size={26} />
+        </a>
+      </div>
+
+      <div className="text-center mt-4 text-sm">
+        <p>&copy; {currentYear} Konstantin Schiwarov. All rights reserved.</p>
       </div>
     </footer>
   );
