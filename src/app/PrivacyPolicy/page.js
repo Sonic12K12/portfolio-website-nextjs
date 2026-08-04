3675098fcaacc31927805c93810310163ce3b6d8
Konstantin S. <k_schiwarov@yahoo.de>
2026-08-04 18:13:23 +0200

    add Privacy Policy component with GDPR compliance details

---
+++ src/app/PrivacyPolicy/page.js
@@ -0,0 +1,444 @@
+export default function PrivacyPolicy() {
+  return (
+    <div id="generator_results">
+      <div id="generator_results_content">
+        <h1>Privacy Policy</h1>
+        <h2>Data Controller</h2>
+        <p>
+          The Data Controller within the meaning of data protection laws, in
+          particular the General Data Protection Regulation (GDPR), is:
+        </p>
+        <p>Konstantin Schiwarov</p>
+
+        <h2>Your Rights as a Data Subject</h2>
+        <p>
+          You can exercise the following rights at any time using the contact
+          details provided in accordance with the General Data Protection
+          Regulation (GDPR):
+        </p>
+        <ul>
+          <li>
+            Information about your data stored by us and its processing (Art. 15
+            GDPR),
+          </li>
+          <li>Correction of incorrect personal data (Art. 16 GDPR),</li>
+          <li>Deletion of your data stored by us (Art. 17 GDPR),</li>
+          <li>
+            Restriction of data processing, provided we are not yet allowed to
+            delete your data due to legal obligations (Art. 18 GDPR),
+          </li>
+          <li>
+            Objection to the processing of your data by us (Art. 21 GDPR) and
+          </li>
+          <li>
+            Data portability, provided you have consented to the data processing
+            or have concluded a contract with us (Art. 20 GDPR).
+          </li>
+        </ul>
+        <p>
+          If you have given us your consent, you can revoke it at any time with
+          effect for the future.
+        </p>
+        <p>
+          You can contact a supervisory authority with a complaint at any time,
+          e.g., the competent supervisory authority of your federal state of
+          residence or the authority responsible for us as the controller.
+        </p>
+        <p>
+          A list of the supervisory authorities (for the non-public sector) with
+          their addresses can be found at:{" "}
+          <a href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html">
+            https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html
+          </a>
+          .
+        </p>
+
+        <h2>Processing Activities</h2>
+
+        <h3>Collection of General Information when Visiting our Website</h3>
+        <h4>Nature and Purpose of Processing</h4>
+        <p>
+          When you access our website, i.e., if you do not register or otherwise
+          submit information, information of a general nature is automatically
+          collected. This information (server log files) includes, for example,
+          the type of web browser, the operating system used, the domain name of
+          your Internet service provider, your IP address, and similar data.
+        </p>
+        <p>They are processed in particular for the following purposes:</p>
+        <ul>
+          <li>Ensuring a smooth connection to the website</li>
+          <li>Ensuring smooth use of our website</li>
+          <li>
+            Ensuring and evaluating system security and stability, in particular
+            for detecting abuse
+          </li>
+          <li>
+            For the technically error-free presentation and optimization of the
+            website
+          </li>
+        </ul>
+        <p>
+          We do not use your data to draw conclusions about your person.
+          However, we reserve the right to retrospectively check the server log
+          files if there are concrete indications of illegal use.
+        </p>
+
+        <h4>Legal Basis and Legitimate Interest</h4>
+        <p>
+          Processing is carried out in accordance with Art. 6 Para. 1 lit. f
+          GDPR on the basis of our legitimate interest in improving the
+          stability and functionality of our website as well as ensuring system
+          security and detecting abuse.
+        </p>
+
+        <h4>Recipients</h4>
+        <p>
+          Recipients of the data may be technical service providers who act as
+          data processors for the operation and maintenance of our website.
+        </p>
+
+        <h4>Retention Period</h4>
+        <p>
+          Data is stored in server log files in a form that allows the
+          identification of data subjects for a maximum of 7 days, unless a
+          security-relevant event occurs (e.g., a DDoS attack).
+        </p>
+        <p>
+          In the event of such an occurrence, server log files will be stored
+          until the security-relevant event has been resolved and completely
+          clarified.
+        </p>
+
+        <h4>Data Transfer to Third Countries</h4>
+        <p>
+          The collected data may be transferred to the following third
+          countries:
+        </p>
+        <p>USA</p>
+        <p>The following data protection guarantees are in place:</p>
+        <p>Adequacy decision of the EU Commission</p>
+
+        <h4>Provision Required or Necessary</h4>
+        <p>
+          The provision of the aforementioned personal data is neither legally
+          nor contractually required. Without the IP address, however, the
+          service and functionality of our website cannot be guaranteed. In
+          addition, individual services and features may not be available or may
+          be limited.
+        </p>
+
+        <h4>Right to Object</h4>
+        <p>
+          Please read the information about your right to object according to
+          Art. 21 GDPR further down below.
+        </p>
+
+        <h3>Contact</h3>
+        <h4>Nature and Purpose of Processing</h4>
+        <p>
+          Contacting us is possible via the provided email addresses. In this
+          case, the user&apos;s personal data transmitted with the email will be
+          stored. This includes the date and time the email was sent, the email
+          address, IP addresses, and information about the servers involved in
+          the email communication.
+        </p>
+        <p>
+          Regardless of the communication method chosen, we collect the content
+          of your inquiry. Your data will be stored for the purpose of
+          individual communication with you.
+        </p>
+
+        <h4>Legal Basis</h4>
+        <p>
+          The processing of the data is based on a legitimate interest (Art. 6
+          Para. 1 lit. f GDPR).
+        </p>
+        <p>
+          Our legitimate interest in processing your data is to facilitate
+          uncomplicated contact.
+        </p>
+        <p>
+          If you contact us to request an offer, the processing of the data is
+          carried out to implement pre-contractual measures (Art. 6 Para. 1 lit.
+          b GDPR).
+        </p>
+
+        <h4>Recipients</h4>
+        <p>
+          Recipients of the data may be technical service providers who act as
+          data processors for the operation and maintenance of our website.
+        </p>
+
+        <h4>Retention Period</h4>
+        <p>
+          Data will be deleted no later than 6 months after the contact request
+          has been processed.
+        </p>
+        <p>
+          If a contractual relationship arises, we are subject to statutory
+          retention periods. These are generally 6 or 10 years for reasons of
+          proper accounting and tax law requirements.
+        </p>
+
+        <h4>Provision Required or Necessary</h4>
+        <p>
+          The provision of your personal data is voluntary. However, we can only
+          process your request if you provide us with the necessary data and the
+          reason for the inquiry.
+        </p>
+
+        <h4>Right to Object</h4>
+        <p>
+          Please read the information about your right to object according to
+          Art. 21 GDPR further down below.
+        </p>
+
+        <h3>Audience Measurement</h3>
+        <h4>Nature and Purpose of Processing</h4>
+        <p>
+          Audience measurement serves to evaluate the flow of visitors to our
+          online offering and can include behavior, interests, or demographic
+          information about visitors, such as age or gender, as pseudonymous
+          values. With the help of reach analysis, we can identify, for example,
+          at what time our online offering or its functions or content are most
+          frequently used or invite reuse. We can also understand which areas
+          need adaptation.
+        </p>
+        <p>
+          You can find out which tools we use for audience measurement below.
+        </p>
+
+        <h4>Legal Basis</h4>
+        <p>
+          Processing is carried out in accordance with Art. 6 Para. 1 lit. f
+          GDPR based on our legitimate interest. The measurement of reach and
+          the resulting information are suitable for adapting the website.
+        </p>
+
+        <h4>Recipients</h4>
+        <p>
+          We use technical service providers for the operation and maintenance
+          of our website, who act as our data processors.
+        </p>
+
+        <h4>Retention Period</h4>
+        <p>
+          Information on the retention period can be found in the information on
+          the tools used listed below.
+        </p>
+
+        <h4>Data Transfer to Third Countries</h4>
+        <p>
+          The collected data may be transferred to the following third
+          countries:
+        </p>
+        <p>USA</p>
+        <p>The following data protection guarantees are in place:</p>
+        <p>Adequacy decision of the EU Commission</p>
+
+        <h4>Provision Required or Necessary</h4>
+        <p>
+          The provision of the data is neither legally nor contractually
+          required.
+        </p>
+
+        <h4>Right to Object</h4>
+        <p>
+          Please read the information about your right to object according to
+          Art. 21 GDPR further down below.
+        </p>
+
+        <h4>Tools Used for Audience Measurement</h4>
+        <h5>Vercel Web Analytics</h5>
+        <p>
+          We use Vercel Web Analytics (Vercel Inc., USA) for anonymized,
+          cookie-free audience measurement. Only general statistical data (such
+          as page views) is recorded, without identifying or tracking individual
+          website visitors.
+        </p>
+
+        <h2>Cookies</h2>
+        <p>
+          A cookie is a small data record that is created when a website is
+          visited and temporarily stored on the website visitor&apos;s system.
+          If the server of this website is called up again by the user of the
+          website, the user&apos;s browser sends the previously received cookie
+          back to the server. The server can evaluate the information obtained
+          through this process. Cookies can, in particular, make it easier to
+          navigate a website.
+        </p>
+        <p>
+          Detailed information on the subject of cookies, and which specific
+          cookies are used on this website and for what purpose, can be found in
+          the sections below.
+        </p>
+
+        <h4>Deleting Cookies</h4>
+        <p>
+          You can delete individual cookies or your entire cookie inventory. In
+          addition, you will receive information and instructions on how these
+          cookies can be deleted or their storage blocked in advance. Depending
+          on the provider of your browser, you will find the necessary
+          information under the following links:
+        </p>
+        <ul>
+          <li>
+            Mozilla Firefox:{" "}
+            <a href="https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen">
+              https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen
+            </a>
+          </li>
+          <li>
+            Microsoft Edge:{" "}
+            <a href="https://support.microsoft.com/de-de/windows/verwalten-von-cookies-in-microsoft-edge-anzeigen-zulassen-blockieren-l%C3%B6schen-und-verwenden-168dab11-0753-043d-7c16-ede5947fc64d">
+              https://support.microsoft.com/de-de/windows/verwalten-von-cookies-in-microsoft-edge-anzeigen-zulassen-blockieren-l%C3%B6schen-und-verwenden-168dab11-0753-043d-7c16-ede5947fc64d
+            </a>
+          </li>
+          <li>
+            Google Chrome:{" "}
+            <a href="https://support.google.com/accounts/answer/61416?hl=de">
+              https://support.google.com/accounts/answer/61416?hl=de
+            </a>
+          </li>
+          <li>
+            Opera:{" "}
+            <a href="http://www.opera.com/de/help">
+              http://www.opera.com/de/help
+            </a>
+          </li>
+          <li>
+            Safari:{" "}
+            <a href="https://support.apple.com/kb/PH17191?locale=de_DE&amp;viewlocale=de_DE">
+              https://support.apple.com/kb/PH17191?locale=de_DE&amp;viewlocale=de_DE
+            </a>
+          </li>
+        </ul>
+        <p>
+          In addition, you can prevent the loading of so-called scripts by
+          default. NoScript allows the execution of JavaScript, Java, and other
+          plugins only for trusted domains of your choice.
+        </p>
+        <p>
+          Information and instructions on how you can edit this function can be
+          obtained from the provider of your browser (e.g., for Mozilla Firefox:{" "}
+          <a href="https://addons.mozilla.org/de/firefox/addon/noscript/">
+            https://addons.mozilla.org/de/firefox/addon/noscript/
+          </a>
+          ).
+        </p>
+
+        <h3>Technically Necessary Cookies</h3>
+        <h4>Nature and Purpose of Processing</h4>
+        <p>
+          We use cookies to make our website more user-friendly. Some elements
+          of our website require that the calling browser can be identified even
+          after a page change.
+        </p>
+        <p>
+          The purpose of using technically necessary cookies is to simplify the
+          use of websites for users. Some functions of our website cannot be
+          offered without the use of cookies. For these, it is necessary that
+          the browser is recognized even after a page change.
+        </p>
+        <p>
+          We exclusively use a technically necessary cookie to ensure the login
+          functionality and to maintain your logged-in status across different
+          browser sessions.
+        </p>
+
+        <h4>Legal Basis and Legitimate Interest</h4>
+        <p>
+          The data processing is carried out solely on the basis of our
+          legitimate interest in a user-friendly design of our website and the
+          documentation of consent pursuant to Art. 6 Para. 1 lit. f GDPR in
+          conjunction with a balancing of interests pursuant to Section 25 Para.
+          2 TDDDG.
+        </p>
+
+        <h4>Recipients</h4>
+        <p>
+          Recipients of the data may be technical service providers who act as
+          data processors for the operation and maintenance of our website.
+        </p>
+
+        <h4>Retention Period</h4>
+        <p>
+          The cookie used for login is persistent and remains on your device
+          even after you close your browser. It will be stored for 30 days or
+          until you manually delete the cookies in your browser settings.
+        </p>
+
+        <h4>Provision Required or Necessary</h4>
+        <p>
+          The provision of the aforementioned personal data is neither legally
+          nor contractually required. Without this data, however, the service
+          and functionality of our website cannot be guaranteed. In addition,
+          individual services and features may not be available or may be
+          limited.
+        </p>
+
+        <h4>Right to Object</h4>
+        <p>
+          Please read the information about your right to object according to
+          Art. 21 GDPR further down below.
+        </p>
+
+        <h2>
+          Information about your Right to Object according to Art. 21 GDPR
+        </h2>
+        <h3>Right to Object in Individual Cases</h3>
+        <p>
+          You have the right, for reasons arising from your particular
+          situation, to object at any time to the processing of personal data
+          concerning you, which is carried out on the basis of Art. 6 Para. 1
+          lit. f GDPR (data processing on the basis of a balancing of
+          interests); this also applies to profiling based on this provision
+          within the meaning of Art. 4 No. 4 GDPR.
+        </p>
+        <p>
+          If you file an objection, we will no longer process your personal data
+          unless we can prove compelling legitimate grounds for the processing
+          that outweigh your interests, rights, and freedoms, or the processing
+          serves to assert, exercise, or defend legal claims.
+        </p>
+
+        <h3>Recipient of an Objection</h3>
+        <p>
+          Konstantin Schiwarov
+          <br />
+          k.schiwarov@gmail.com
+        </p>
+
+        <h2>Changes to our Privacy Policy</h2>
+        <p>
+          We reserve the right to adapt this privacy policy so that it always
+          complies with current legal requirements or to implement changes to
+          our services in the privacy policy, e.g., when introducing new
+          services. The new privacy policy will then apply to your next visit.
+        </p>
+
+        <h2>Questions about Data Protection</h2>
+        <p>
+          If you have any questions about data protection, please write us an
+          email to the Data Controller named above.
+        </p>
+
+        <h2>Copyright Information</h2>
+        <p>
+          <em>
+            This privacy policy was created with the help of activeMind AG – the
+            experts for{" "}
+            <a
+              href="https://www.activemind.de/datenschutz/datenschutzbeauftragter/"
+              target="_blank"
+              rel="noopener dofollow"
+            >
+              external data protection officers
+            </a>{" "}
+            (Version #2024-10-25).
+          </em>
+        </p>
+      </div>
+    </div>
+  );
+}
