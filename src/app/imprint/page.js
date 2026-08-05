5a97a6da545e20ce611306c9e2af5e3df745e9f1
Konstantin Schiwarov <k_schiwarov@yahoo.de>
2026-08-05 12:21:09 +0200

    add Imprint page

---
+++ src/app/imprint/page.js
@@ -0,0 +1,49 @@
+export default function ImprintPage() {
+  return (
+    // Wrapper with Tailwind typography classes
+    <main className="prose prose-invert max-w-none p-10 w-3/4 mx-auto">
+      <div>
+        <h1>Legal Notice</h1>
+
+        <h2>Responsible Person</h2>
+        <p>Konstantin Schiwarov</p>
+
+        <h3>Postal Address</h3>
+        <p>
+          {/* Environment variables for postal address */}
+          {process.env.NEXT_PUBLIC_STREET}
+          <br />
+          {process.env.NEXT_PUBLIC_CITY}
+        </p>
+
+        <h3>Contact</h3>
+        <p>Email: {process.env.NEXT_PUBLIC_EMAIL}</p>
+
+        <h2>Website Information</h2>
+
+        <h3>Copyright Notice</h3>
+        <p>
+          All images, photographs, and graphics used on this website are
+          protected by copyright and were created by the website operator.
+        </p>
+
+        <h3>Responsible for editorial content</h3>
+        <p>Konstantin Schiwarov</p>
+
+        <p>
+          <em>
+            This Legal Notice was created with the help of the{" "}
+            <a
+              href="https://www.activemind.de/generatoren/impressum/"
+              target="_blank"
+              rel="noopener"
+            >
+              Legal Notice Generator from activeMind AG
+            </a>{" "}
+            (Version 2024-07-21).
+          </em>
+        </p>
+      </div>
+    </main>
+  );
+}
