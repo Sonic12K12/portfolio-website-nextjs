4150311dda1d3dee50a6b82d6d7e1f9f00cac5f6
Konstantin S. <k_schiwarov@yahoo.de>
2026-08-16 13:07:50 +0200

    update introduction paragraph and button styling in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -8,16 +8,17 @@ export default function Home() {
         Hi, I&apos;m Konstantin.
       </h1>
       <p className="text-text-main text-lg md:text-xl max-w-2xl text-center">
-        With a strong foundation in Java from my Media and Computing studies,
-        I&apos;m currently diving deep into modern web development.
+        My background in Media and Computing at HTW Berlin gave me a solid
+        foundation in Java. Now, I&apos;m fully focused on diving into modern
+        web development.
       </p>
       <div>
         <Link href="/projects">
           <button
             type="button"
-            className="bg-primary text-3xl rounded-xl border-none shadow-xl cursor-pointer text-black"
+            className="bg-primary text-3xl rounded-2xl py-8 px-10 mt-8 border-none shadow-xl cursor-pointer text-black"
           >
-            Button
+            <span className="[font-variant-ligatures:none]">{"-->"}</span>
           </button>
         </Link>
       </div>
