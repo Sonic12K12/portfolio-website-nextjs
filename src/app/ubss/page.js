428dd171e3497d99421daf1d0763371b7d097f14
Konstantin S. <k_schiwarov@yahoo.de>
2026-09-01 19:07:50 +0200

    update UBSS page with additional content sections

--- src/app/ubss/page.js
+++ src/app/ubss/page.js
@@ -6,7 +6,7 @@ export default function UBSSPage() {
       <header>
         <h1 className="mb-12">Urban Bio Space Simulator</h1>
         <Image
-          src="/img/ubbs_image_1.webp"
+          src="/img/ubss/ubss_image_1.webp"
           alt="Screenshot of BioTopia"
           className="rounded-xl"
           width={1920}
@@ -101,13 +101,13 @@ export default function UBSSPage() {
           <strong>Procedural Growth:</strong> The city expands procedurally,
           simulating the rapidly decreasing available space in urban areas.
         </li>
-        <li className="list-none">
+        <li className="list-none -ml-6">
           <video
             src="/videos/BioTopia_demo_loop.mp4"
             muted
             playsInline
             controls
-            className="w-xl rounded-xl my-12 mx-auto"
+            className="w-full mx-auto rounded-xl my-12"
           >
             {/* Fallback text in case a user has an extremely outdated browser */}
             Your browser does not support the video tag.
@@ -121,9 +121,41 @@ export default function UBSSPage() {
         <li>
           <strong>Real-World Science:</strong> Designed to showcase the research
           of <em>food4future</em>, core gameplay elements are directly based on
-          actual research for sustainable food production in urban enviroments.
+          actual research for sustainable food production in urban environments.
         </li>
       </ul>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mt-12">
+        <Image
+          src="/img/ubss/ubss_image_2.webp"
+          alt="BioTopia ingame screenshot"
+          width={2340}
+          height={1080}
+          className="rounded-xl col-span-1 md:col-span-3"
+        />
+        <Image
+          src="/img/ubss/ubss_image_3.webp"
+          alt="BioTopia ingame screenshot"
+          width={1170}
+          height={540}
+          className="rounded-xl"
+        />
+        <Image
+          src="/img/ubss/ubss_image_4.webp"
+          alt="BioTopia ingame screenshot"
+          width={1170}
+          height={540}
+          className="rounded-xl"
+        />
+        <Image
+          src="/img/ubss/ubss_image_5.webp"
+          alt="BioTopia ingame screenshot"
+          width={1170}
+          height={540}
+          className="rounded-xl"
+        />
+      </div>
+
       <hr className="border-t border-muted w-full my-12 mx-auto" />
 
       <h2 className="mb-4 mt-8">My Role & Process</h2>
@@ -160,6 +192,108 @@ export default function UBSSPage() {
           communication routine, meeting twice a week: once internally as a
           student team to align on progress, and once with our project
           supervisor for feedback and direction.
+        </li>
+      </ul>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-3 mt-12">
+        <Image
+          src="/img/ubss/algae_comp_lowP.webp"
+          alt="algae compartment low-poly"
+          width={965}
+          height={540}
+          className="rounded-xl w-full h-full object-cover aspect-4/3"
+        />
+        <Image
+          src="/img/ubss/jellyfish_comp.webp"
+          alt="jellyfish compartment"
+          width={965}
+          height={540}
+          className="rounded-xl w-full h-full object-cover aspect-4/3"
+        />
+        <Image
+          src="/img/ubss/crickets_comp.webp"
+          alt="crickets compartment"
+          width={965}
+          height={540}
+          className="rounded-xl w-full h-full object-cover aspect-4/3"
+        />
+
+        <Image
+          src="/img/ubss/algae_comp.webp"
+          alt="algae compartment"
+          width={965}
+          height={540}
+          className="rounded-xl w-full h-full object-cover aspect-4/3"
+        />
+      </div>
+
+      <hr className="border-t border-muted w-full my-12 mx-auto" />
+
+      <h2 className="mb-4">Public Pitch & Exhibition</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <p>
+          A major highlight of the development process was pitching the game at
+          the food4future summer festival at Therme Bad Saarow in July 2025. I
+          had the opportunity to deliver the main presentation, introducing our
+          core concepts and mechanics to an audience of stakeholders and domain
+          experts. Following the talk, we showcased a playable prototype at our
+          booth, gathering invaluable feedback. This direct exchange heavily
+          influenced our final polish leading up to the official HTW IMI
+          Showtime exhibition.
+        </p>
+
+        <Image
+          src="/img/ubss/ubss_bad_saarow_presentation.webp"
+          alt="presentation of BioTopia in Bad Saarow"
+          width={874}
+          height={636}
+          className="rounded-xl mt-2.5"
+        />
+      </div>
+
+      <hr className="border-t border-muted w-full my-12 mx-auto" />
+
+      <h2 className="mb-4">Retrospective</h2>
+      <p>
+        Working on BioTopia was a great experience in balancing technical limits
+        with an engaging user experience. Coming up with a solid game concept
+        from scratch was a challenge in itself, especially since we had to turn
+        real research from food4future into intuitive gameplay mechanics while
+        also keeping our 3D assets optimized for mobile devices. But beyond the
+        tech side, it was just incredibly cool to go through a full production
+        cycle—from the first brainstorming session to actually playing a real,
+        finished game. Our team effort really paid off with a{" "}
+        <strong>final grade of 1.3</strong>, and it showed me how well serious
+        games can work to make science accessible and engaging.
+      </p>
+
+      <hr className="border-t border-muted w-full my-12 mx-auto" />
+
+      <h3 className="text-xl mb-4">Project Links</h3>
+
+      <ul className="flex flex-col space-y-3">
+        <li>
+          <a
+            href="https://showtime.f4.htw-berlin.de/ss25/bachelor/b4-urban-bio-space-simulator/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline underline-offset-4 hover:text-primary transition-colors text-text-main"
+          >
+            Urban Bio Space Simulator (BioTopia) Project Page
+            <span className="inline-block ml-1.5">↗</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.food4future.de/en/home"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline underline-offset-4 hover:text-primary transition-colors text-text-main"
+          >
+            food4future Initiative
+            <span className="inline-block ml-1.5 ">↗</span>
+          </a>
         </li>
       </ul>
     </article>
