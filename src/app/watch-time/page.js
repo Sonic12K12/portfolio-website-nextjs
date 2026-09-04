3cf9beb5aa448fc33feb5656a190b7fcd851dd3e
Konstantin S. <k_schiwarov@yahoo.de>
2026-09-04 11:35:30 +0200

    reduce x padding

--- src/app/watch-time/page.js
+++ src/app/watch-time/page.js
@@ -1,6 +1,6 @@
 export default function WatchTimePage() {
   return (
-    <main className="max-w-3xl mx-auto px-6">
+    <main className="max-w-3xl mx-auto px-4">
       <header>
         <h1>Comming Soon 🍿</h1>
       </header>
